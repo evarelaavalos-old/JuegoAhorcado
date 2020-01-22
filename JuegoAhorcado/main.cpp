afcc9e53ba04d007f49485651e3fefcdceb8752f
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2020-01-22 17:38:27 -0300

    extension de la introduccion y se añadio pseudocodigo

--- JuegoAhorcado/main.cpp
+++ JuegoAhorcado/main.cpp
@@ -1,27 +1,36 @@
 /* La idea original era hacerlo en ingles, pero se me dificulto mucho
-el idioma. Solo me entorpecia el trabajo */
+el idioma. Solo me entorpecia el trabajo, así que quedará pendiente
+para más adelante */
 
 #include <iostream>
 #include <string>
 
+constexpr int LONGITUD_PALABRA = 7;
+constexpr int NUMERO_INTENTOS = 5;
+
 int main()
 {
-	constexpr int LONGITUD_PALABRA = 6;
+	// introduciendo el juego
+	std::cout << "Bienvenido al juego del Ahorcado! ";
+	std::cout << "Un divertido juego de palabras!\n";
 	std::cout << "Podras adivinar esta palabra de " << LONGITUD_PALABRA;
-	std::cout << " letras?\n";
+	std::cout << " letras antes de agotar los " << NUMERO_INTENTOS;
+	std::cout << " intentos?\n";
 	std::cout << std::endl;
 
-	constexpr int NUMERO_INTENTOS = 6;
+	// palabra a adivinar
+	const std::string PalabraOculta = "Raqueta";
 
-	for (int i = 1; i <= NUMERO_INTENTOS; i++)
+	// obtener una letra valida por cada uno de los turnos
+	for (int IntentoActual = 1; IntentoActual <= NUMERO_INTENTOS; IntentoActual++)
 	{
-		std::string LetraSupuesta;
+		// todo mostrar el numero de intento y las letras que vaya descubriendo
 
-		std::cout << "Intento " << i << ". ";
+		// obteniendo una letra del jugador
+		std::string LetraSupuesta;
+		std::cout << "Ingrese una letra: ";
 		getline(std::cin, LetraSupuesta);
 
 		std::cout << std::endl;
-
 	}
-
 }
