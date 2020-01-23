15f969a948c503d6fcc0c734b2c697521f3eac75
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2020-01-22 21:21:45 -0300

    introduciendo funciones

--- JuegoAhorcado/main.cpp
+++ JuegoAhorcado/main.cpp
@@ -5,18 +5,16 @@ para más adelante */
 #include <iostream>
 #include <string>
 
+void MostrarIntro();
+std::string ObtenerLetra();
+
 constexpr int LONGITUD_PALABRA = 7;
 constexpr int NUMERO_INTENTOS = 5;
 
+// el punto de entrada para nuestra aplicacion
 int main()
 {
-	// introduciendo el juego
-	std::cout << "Bienvenido al juego del Ahorcado! ";
-	std::cout << "Un divertido juego de palabras!\n";
-	std::cout << "Podras adivinar esta palabra de " << LONGITUD_PALABRA;
-	std::cout << " letras antes de agotar los " << NUMERO_INTENTOS;
-	std::cout << " intentos?\n";
-	std::cout << std::endl;
+	MostrarIntro();
 
 	// palabra a adivinar
 	const std::string PalabraOculta = "Raqueta";
@@ -25,12 +23,31 @@ int main()
 	for (int IntentoActual = 1; IntentoActual <= NUMERO_INTENTOS; IntentoActual++)
 	{
 		// todo mostrar el numero de intento y las letras que vaya descubriendo
-
-		// obteniendo una letra del jugador
-		std::string LetraSupuesta;
-		std::cout << "Ingrese una letra: ";
-		getline(std::cin, LetraSupuesta);
+		ObtenerLetra();
 
 		std::cout << std::endl;
 	}
+
+	return 0;
+}
+
+// introduciendo el juego
+void MostrarIntro()
+{
+	std::cout << "Bienvenido al juego del Ahorcado! ";
+	std::cout << "Un divertido juego de palabras!\n";
+	std::cout << "Podras adivinar esta palabra de " << LONGITUD_PALABRA;
+	std::cout << " letras antes de agotar los " << NUMERO_INTENTOS;
+	std::cout << " intentos?\n";
+	std::cout << std::endl;
+}
+
+// obteniendo una letra del jugador
+std::string ObtenerLetra()
+{
+	std::string LetraSupuesta;
+	std::cout << "Ingrese una letra: ";
+	getline(std::cin, LetraSupuesta);
+
+	return LetraSupuesta;
 }
